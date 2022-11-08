d5c3ab57f2bc9ccf3601f52ebb82fa145c01a167
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-08 12:39:29 -0800

    Add icons to buttons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,9 +11,11 @@ function Header(){
             </div>
             <div className = "header-bottom">
                 <button className = "email-btn">
+                    <i className="fa-solid fa-envelope"></i>
                     Email
                 </button>
                 <button className = "linkedin-btn">
+                    <i className="fa-brands fa-linkedin"></i>
                     LinkedIn
                 </button>
             </div>
