02b5c0ff2341e85f2e4ce751f0dba1e821f1f2be
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-08 11:44:13 -0800

    More CSS, add header-text

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,15 @@ import React, { Component } from "react"
 
 function Header(){
     return (
-         <div className = "main-photo">
-         </div>
+        <header>
+            <div className = "main-photo"></div>
+            <div className = "header-text">
+                <h1>Malcolm Smalls</h1>
+                <span className = "title-text">Frontend Developer</span>
+                <span className = "website-text">malcolmsmalls.website</span>
+            </div>
+        </header>
+
     )
 }
 
